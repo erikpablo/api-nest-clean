9430b18357483b0711b0be58a54ce9c58b178f81
Erik Pablo <nunespablo470@gmail.com>
2025-11-04 16:58:02 -0300

    feat: :sparkles: config repository

--- src/infra/database/prisma/repositories/prisma-answers-comments-repository.ts
+++ src/infra/database/prisma/repositories/prisma-answers-comments-repository.ts
@@ -1,8 +1,8 @@
-import type { PaginationParams } from '@/core/repositories/pagination-params'
+import { PaginationParams } from '@/core/repositories/pagination-params'
 import { AnswerCommentsRepository } from '@/domain/forum/application/repositories/answer-comments-repository'
 import { AnswerComment } from '@/domain/forum/enterprise/entities/answer-comment'
 import { Injectable } from '@nestjs/common'
-import type { PrismaService } from '../prisma.service'
+import { PrismaService } from '../prisma.service'
 import { PrismaAnswerCommentMapper } from '../mappers/prisma-answer-comment-mapper'
 
 @Injectable()
